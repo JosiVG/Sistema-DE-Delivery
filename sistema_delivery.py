4e03e8312a025ca269fc488f7ad351aa671413a2
alejoromeroPasetto <alejoromeroutn@gmail.com>
2026-07-01 20:40:55 -0300

    Validacion de zona

--- sistema_delivery.py
+++ sistema_delivery.py
@@ -1,3 +1,6 @@
+TRABAJO FINAL INTEGRADOR - SISTEMA DE DELIVERY
+# INTEGRANTES: [Romer Pasetto, Alejo, ] 
+# COMISION: [K1.4]
 total_pedidos = 0
 pedidos_entregados = 0
 pedidos_pendientes = 0
@@ -22,3 +25,17 @@ def validar_monto():
                 print("? Error: El monto debe ser mayor a cero. Intente de nuevo.")
         else:
             print("? Error: Entrada inválida. Ingrese solo números (ej: 1450 o 350.50).")
+
+        def validar_zona():
+    """Valida que la zona ingresada sea correcta."""
+    continuar_validacion = True
+    while continuar_validacion:
+    print("Zonas de reparto: [N]orte | [S]ur | [C]entro")
+        zona = input("Seleccione la zona (N/S/C): ").upper().strip()
+        
+    # Condicional para verificar la opción correcta
+        if zona == "N" or zona == "S" or zona == "C":
+            return zona
+        else:
+            print("? Error")
+
